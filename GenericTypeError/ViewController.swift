283811546b81426a9db6db1bfd8087516b9c5581
Kris Woodall <kriswoodall@me.com>
2020-04-10 09:31:32 -0500

    Initial Commit

---
+++ GenericTypeError/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericTypeError
+//
+//  Created by Kristopher Woodall on 4/10/20.
+//  Copyright © 2020 Remind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
